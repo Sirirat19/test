f8531c1c660f2eff9f3aeaf01466f413b37d00c1
Sirirat19 <sirirat150261@gmail.com>
2024-06-13 10:06:52 +0700

    Add files via upload

---
+++ main.js.js
@@ -0,0 +1,6 @@
+function mul() {
+    let number1 = document.getElementById("number1") ;
+    let result = document.getElementById("result");
+    let sum = parseFloat(number1.value) *  0.1974;
+    result.innerHTML = sum;
+}
